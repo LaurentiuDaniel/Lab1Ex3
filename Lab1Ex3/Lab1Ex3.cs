2d821b733d0a4126fd4c9186c1ae24d10c9aab32
Dani <amaximoaiei.l@yahoo.com>
2023-06-23 13:49:37 +0300

    Add project files.

---
+++ Lab1Ex3/Lab1Ex3.cs
@@ -0,0 +1,21 @@
+﻿/*Ex3
+Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la
+tastaura*/
+
+class Lab1Ex3 {
+    static void Main() 
+    {
+
+        int numar;
+
+        Console.WriteLine("Introdu un numar intreg:");
+            numar = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Ultimul numar este:");
+
+        Console.WriteLine(numar % 10);
+    
+    }
+
+}
+
